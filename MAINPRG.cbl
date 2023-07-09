4a60e5df5447103abb7394e54511613a53a323a9
aysuoner <aysuoner5@gmail.com>
2023-07-10 00:13:03 +0300

    fixed inp-key value

--- MAINPRG.cbl
+++ MAINPRG.cbl
@@ -19,8 +19,9 @@
        FD  INP-FILE RECORDING MODE F.
        01  INP-REC.
 		     05 INP-TYPE       PIC X(01).
-           05 INP-ID         PIC 9(05) COMP-3.
-           05 INP-DVZ        PIC 9(03) COMP.
+           05 INP-KEY.
+            07 INP-ID         PIC 9(05) COMP-3.
+            07 INP-DVZ        PIC 9(03) COMP.
       *****
        WORKING-STORAGE SECTION.
        01  EXIT-FLAG         PIC X(01) VALUE 'N'.
@@ -28,6 +29,15 @@
            05 INP-ST         PIC 9(02).
               88 INP-EOF     VALUE 10.
               88 INP-SUCCESS VALUE 00 97.
+       01  SUB-AREA.
+           05 PROC-TYPE    PIC X(01).
+             88 READ-TYPE   VALUE 'R'.
+             88 WRITE-TYPE  VALUE 'W'.
+             88 UPDTE-TYPE  VALUE 'U'.
+             88 DELT-TYPE   VALUE 'D'.
+           05 SUB-IDX-KEY.
+             07 SUB-IDX-ID   PIC 9(05) COMP-3.
+             07 SUB-IDX-DVZ  PIC 9(03) COMP.
       *--------------------
         PROCEDURE DIVISION.
       *--------------------
@@ -35,7 +45,7 @@
       *----
        FILE-OPEN-CONTROL.
            IF NOT INP-SUCCESS
-             DISPLAY 'FILE CANNOT OPEN'
+             DISPLAY 'FILEM CANNOT OPEN'
              MOVE 'Y' TO EXIT-FLAG
              MOVE 99 TO RETURN-CODE
              PERFORM PROGRAM-EXIT
@@ -45,7 +55,11 @@
        READ-INP-FILE.
            READ INP-FILE
            PERFORM UNTIL INP-EOF
-              CALL 'SUBPRG' USING INP-TYPE, INP-ID, INP-DVZ
+              MOVE INP-TYPE TO PROC-TYPE
+              MOVE INP-ID   TO SUB-IDX-ID
+              MOVE INP-DVZ  TO SUB-IDX-DVZ
+              MOVE INP-KEY  TO SUB-IDX-KEY
+              CALL 'SUBPRG' USING SUB-AREA
               READ INP-FILE
            END-PERFORM.
            MOVE 'Y' TO EXIT-FLAG
