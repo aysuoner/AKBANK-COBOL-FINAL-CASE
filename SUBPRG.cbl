7cfdc0978e9073f5a9b3bf83c8f39a0d990188d0
aysuoner <aysuoner5@gmail.com>
2023-07-10 00:15:53 +0300

    added type-detect,readproc and writeproc parag

--- SUBPRG.cbl
+++ SUBPRG.cbl
@@ -20,78 +20,125 @@
        FD  IDX-FILE.
        01  IDX-REC.
            05 IDX-KEY.
-              10  IDX-ID    PIC 9(05) COMP-3.
-              10  IDX-DVZ   PIC 9(03) COMP.
+              10  IDX-ID    PIC 9(05)    COMP-3.
+              10  IDX-DVZ   PIC 9(03)    COMP.
            05 IDX-FIRSTN    PIC X(15).
            05 IDX-LASTN     PIC X(15).
-           05 IDX-JUL       PIC 9(07) COMP-3.
+           05 IDX-JUL       PIC 9(07)    COMP-3.
            05 IDX-TUTAR     PIC 9(13)V99 COMP-3.
        WORKING-STORAGE SECTION.
-       01 IDX-ST            PIC 9(02).
-           88 IDX-SUCCESS   VALUE 00 97.
-           88 DUPLICATE     VALUE 22.
+       01  EXIT-FLAG            PIC X(01) VALUE 'N'.
+       01  FILE-FLAGS.
+           05 IDX-ST            PIC 9(02).
+               88 IDX-SUCCESS   VALUE 00 41 97.
+               88 DUPLICATE     VALUE 22.
        LINKAGE SECTION.
-       01  LN-INP-REC.
-		     05 LN-INP-TYPE   PIC X(01).
-		       88 READ-TYPE     VALUE 'R'.
-		       88 WRITE-TYPE    VALUE 'W'.
-		       88 UPDTE-TYPE    VALUE 'U'.
-		       88 DELT-TYPE     VALUE 'D'.
-           05 LN-INP-ID     PIC 9(05) COMP-3.
-           05 LN-INP-DVZ    PIC 9(03) COMP.
-       01  LN-WS-TYPE       PIC X(01).
-       PROCEDURE DIVISION USING LN-INP-REC, LN-WS-TYPE.
-           OPEN INPUT IDX-FILE.
-       TYPE-CALL.
-           DISPLAY "LNINP:" LN-INP-TYPE.
+       01  LN-SUB-AREA.
+           05 LN-PROC-TYPE     PIC X(01).
+             88 LN-READ-TYPE   VALUE 'R'.
+             88 LN-WRITE-TYPE  VALUE 'W'.
+             88 LN-UPDTE-TYPE  VALUE 'U'.
+             88 LN-DELT-TYPE   VALUE 'D'.
+           05 LN-SUB-IDX-KEY.
+            07 LN-SUB-IDX-ID   PIC 9(05) COMP-3.
+            07 LN-SUB-IDX-DVZ  PIC 9(03) COMP.
+      *------------
+       PROCEDURE DIVISION USING LN-SUB-AREA.
+       MAIN-PRAG.
+           PERFORM FILE-OPEN-CONTROL
+           PERFORM SET-IDX-KEY
+           PERFORM TYPE-DETECT
+           INITIALIZE LN-SUB-AREA
+           PERFORM PROGRAM-EXIT.
+       MAIN-PRAG-END. EXIT.
+      *----
+       FILE-OPEN-CONTROL.
+           OPEN I-O IDX-FILE
+           IF NOT IDX-SUCCESS
+              DISPLAY 'FILEX CANNOT OPEN: ' IDX-ST
+              MOVE 'Y' TO EXIT-FLAG
+              MOVE IDX-ST TO RETURN-CODE
+              PERFORM PROGRAM-EXIT
+           END-IF.
+       FILE-OPEN-CONTROL-END. EXIT.
+      *----
+       SET-IDX-KEY.
+           MOVE LN-SUB-IDX-ID  TO IDX-ID
+           MOVE LN-SUB-IDX-DVZ TO IDX-DVZ
+           MOVE LN-SUB-IDX-KEY TO IDX-KEY.
+       SET-IDX-KEY-END. EXIT.
+      *----
+       TYPE-DETECT.
+           DISPLAY "LNINP:" LN-PROC-TYPE
            EVALUATE TRUE
-           WHEN READ-TYPE
-            PERFORM READ-SUB
-            EXIT PROGRAM
-           WHEN WRITE-TYPE
-            PERFORM WRITE-SUB
-            EXIT PROGRAM
-           WHEN UPDTE-TYPE
-            PERFORM UPDTE-SUB
-            EXIT PROGRAM
-           WHEN DELT-TYPE
-            PERFORM DELT-SUB
-            EXIT PROGRAM
+           WHEN LN-READ-TYPE
+            PERFORM READ-PROCESS
+           WHEN LN-WRITE-TYPE
+            PERFORM WRITE-PROCESS
+           WHEN LN-UPDTE-TYPE
+            PERFORM UPDTE-PROCESS
+           WHEN LN-DELT-TYPE
+            PERFORM DELT-PROCESS
            WHEN OTHER
             DISPLAY 'GECERSIZ ISTEK'
-           EXIT PROGRAM
            END-EVALUATE.
-       TYPE-CALL-END. EXIT.
-      *
-       READ-SUB.
-           DISPLAY 'READ'.
-      *     MOVE LN-INP-ID  TO IDX-ID
-      *     MOVE LN-INP-DVZ TO IDX-DVZ
-      *     MOVE LN-INP-REC TO IDX-KEY.
-      *     READ IDX-FILE KEY IS IDX-KEY
-      *     INVALID KEY
-      *      DISPLAY "Record Undefined: " LN-INP-ID LN-INP-DVZ
-      *     NOT INVALID KEY
-      *      DISPLAY "Record OK: " LN-INP-ID LN-INP-DVZ.
-       READ-SUB-END. EXIT.
-      *
-       WRITE-SUB.
-           DISPLAY 'WRITE'.
-       WRITE-SUB-END. EXIT.
-      *
-       UPDTE-SUB.
+       TYPE-DETECT-END. EXIT.
+      *----
+      *----
+      *----
+      *----
+       READ-PROCESS.
+           DISPLAY 'READ'
+           READ IDX-FILE KEY IS IDX-KEY
+           INVALID KEY
+            DISPLAY 'Record Undefined: '
+           NOT INVALID KEY
+            DISPLAY 'Record OK: '.
+       READ-PROCESS-END. EXIT.
+      *----
+      *----
+      *----
+      *----
+       WRITE-PROCESS.
+           DISPLAY 'WRITE'
+           READ IDX-FILE KEY IS IDX-KEY
+           INVALID KEY
+              MOVE 'AYSU           ' TO IDX-FIRSTN
+              MOVE 'ONER           ' TO IDX-LASTN
+              MOVE '1995126' TO IDX-JUL
+              MOVE '000000000000000' TO IDX-TUTAR
+              WRITE IDX-REC
+              DISPLAY 'ADDED NEW RECORD'
+           NOT INVALID KEY
+              DISPLAY 'DUPLICATE ERROR FOR WRITE: ' IDX-FIRSTN IDX-LASTN 
+           END-READ.
+       WRITE-PROCESS-END. EXIT.
+      *----
+      *----
+      *----
+      *----
+       UPDTE-PROCESS.
            DISPLAY  'UPDATE'.
-       UPDTE-SUB-END. EXIT.
-      *
-       DELT-SUB.
+       UPDTE-PROCESS-END. EXIT.
+      *----
+      *----
+      *----
+      *----
+       DELT-PROCESS.
            DISPLAY  'DELT'.
-       DELT-SUB-END. EXIT.
+       DELT-PROCESS-END. EXIT.
       *
-      * SET-IDX-KEY.
-      *     MOVE LN-INP-ID  TO IDX-ID
-      *     MOVE LN-INP-DVZ TO IDX-DVZ
-      *     MOVE LN-INP-REC TO IDX-KEY.
-      * SET-IDX-KEY-END. EXIT.
-
-           GOBACK.
+      *
+      *
+      *
+      *
+      *
+       PROGRAM-EXIT.
+           IF EXIT-FLAG = 'Y' THEN
+               CLOSE IDX-FILE
+               EXIT PROGRAM
+           ELSE
+              EXIT PROGRAM
+           END-IF.
+       PROGRAM-EXIT-END.
        END PROGRAM SUBPRG.
