ee38d78454ed431ad09db64c4e33476e4bcfbb1f
aysuoner <aysuoner5@gmail.com>
2023-07-13 22:30:17 +0300

    READABILITY INCREASED V2.

--- MAINPRG.cbl
+++ MAINPRG.cbl
@@ -20,55 +20,55 @@
        FILE SECTION.
        FD  INP-FILE RECORDING MODE F.
        01  INP-REC.
-		     05 INP-TYPE           PIC X(01).
-             88 READ-TYPE                 VALUE 'R'.
-             88 WRITE-TYPE                VALUE 'W'.
-             88 UPDTE-TYPE                VALUE 'U'.
-             88 DELT-TYPE                 VALUE 'D'.
+		     05 INP-TYPE            PIC X(01).
+             88 READ-TYPE                  VALUE 'R'.
+             88 WRITE-TYPE                 VALUE 'W'.
+             88 UPDTE-TYPE                 VALUE 'U'.
+             88 DELT-TYPE                  VALUE 'D'.
            05 INP-KEY.
-             10 INP-ID           PIC 9(05) COMP-3.
-             10 INP-DVZ          PIC 9(03) COMP.
+             10 INP-ID            PIC 9(05) COMP-3.
+             10 INP-DVZ           PIC 9(03) COMP.
       *****
        FD  OUT-FILE RECORDING MODE F.
        01  OUT-REC.
            05 OUT-KEY-INFO.
-             10 OUT-ID           PIC 9(05).
-             10 OUT-DVZ          PIC 9(03).
+             10 OUT-ID            PIC 9(05).
+             10 OUT-DVZ           PIC 9(03).
            05 OUT-MSG-INFO.
-             10 OUT-RROC-TYP     PIC X(09).
-             10 OUT-RC           PIC 9(02).
-             10 OUT-MSG          PIC X(20).
-             10 OUT-FROM         PIC X(36).
-             10 OUT-TO           PIC X(34).
+             10 OUT-RROC-TYP      PIC X(09).
+             10 OUT-RC            PIC 9(02).
+             10 OUT-MSG           PIC X(20).
+             10 OUT-FROM          PIC X(36).
+             10 OUT-TO            PIC X(34).
       *****
        WORKING-STORAGE SECTION.
        01  FILE-FLAGS.
-           05 INP-ST             PIC 9(02).
-             88 INP-EOF                   VALUE 10.
-             88 INP-SUCCESS               VALUE 00 97.
-           05 OUT-ST             PIC 9(02).
-             88 OUT-SUCCESS               VALUE 00 97.
-           05 EXIT-ST            PIC X(01).
-             88 EXIT-FLAG                 VALUE 'Y'.
+           05 INP-ST              PIC 9(02).
+             88 INP-EOF                    VALUE 10.
+             88 INP-SUCCESS                VALUE 00 97.
+           05 OUT-ST              PIC 9(02).
+             88 OUT-SUCCESS                VALUE 00 97.
+           05 EXIT-ST             PIC X(01).
+             88 CLOSE-FILE                 VALUE 'Y'.
        01  SUB-AREA.
            05 SUB-IDX-KEY.
-             10 SUB-IDX-ID       PIC 9(05) COMP-3.
-             10 SUB-IDX-DVZ      PIC 9(03) COMP.
+             10 SUB-IDX-ID        PIC 9(05) COMP-3.
+             10 SUB-IDX-DVZ       PIC 9(03) COMP.
            05 SUB-OUT-MSG-INFO.
-             10 SUB-OUT-RROC-TYP PIC X(09).
-             10 SUB-OUT-RC       PIC 9(02).
-             10 SUB-OUT-MSG      PIC X(20).
-             10 SUB-FIRSTNFROM   PIC X(15).
-             10 SUB-FIRSTNTO     PIC X(15).
-             10 SUB-LASTNFROM    PIC X(15).
-             10 SUB-LASTNTO      PIC X(15).
+             10 SUB-OUT-RROC-TYP  PIC X(09).
+             10 SUB-OUT-RC        PIC 9(02).
+             10 SUB-OUT-MSG       PIC X(20).
+             10 SUB-FIRSTNFROM    PIC X(15).
+             10 SUB-FIRSTNTO      PIC X(15).
+             10 SUB-LASTNFROM     PIC X(15).
+             10 SUB-LASTNTO       PIC X(15).
       *--------------------
         PROCEDURE DIVISION.
       *--------------------
        MAIN-PRAG.
            PERFORM FILE-OPEN-CONTROL
            PERFORM READ-INP-FILE
-           SET EXIT-FLAG TO TRUE
+           SET CLOSE-FILE TO TRUE
            MOVE 00 TO RETURN-CODE
            PERFORM PROGRAM-EXIT.
        MAIN-PRAG-END. EXIT.
@@ -81,7 +81,7 @@
             DISPLAY 'FILE CANNOT OPEN'
             DISPLAY 'INP-ST: ' INP-ST
             DISPLAY 'OUT-ST: ' OUT-ST
-            SET EXIT-FLAG TO TRUE
+            SET CLOSE-FILE TO TRUE
             MOVE 99 TO RETURN-CODE
             PERFORM PROGRAM-EXIT
            END-IF.
@@ -99,7 +99,7 @@
             MOVE SPACES   TO OUT-TO
             PERFORM SUB-PROG-HANDLE
             PERFORM PRIN-OUT-FILE
-            CALL 'PRGEXIT'
+      *      CALL 'PRGEXIT'
             READ INP-FILE
            END-PERFORM.
        READ-INP-FILE-END. EXIT.
@@ -125,9 +125,9 @@
       *----
        PRIN-OUT-FILE.
            MOVE SUB-OUT-RROC-TYP TO OUT-RROC-TYP
-           MOVE SUB-OUT-MSG TO OUT-MSG 
+           MOVE SUB-OUT-MSG TO OUT-MSG
            MOVE SUB-OUT-RC TO OUT-RC
-           IF INP-TYPE IS EQUAL TO  'U' AND OUT-RC IS ZERO 
+           IF INP-TYPE IS EQUAL TO  'U' AND OUT-RC IS ZERO
             STRING 'FROM: ' DELIMITED BY SIZE
                    SUB-FIRSTNFROM DELIMITED BY SIZE
                    SUB-LASTNFROM DELIMITED BY SIZE
@@ -147,7 +147,7 @@
       *----
       *----
        PROGRAM-EXIT.
-           IF EXIT-FLAG
+           IF CLOSE-FILE
                CLOSE INP-FILE
                CLOSE OUT-FILE
                STOP RUN
@@ -155,7 +155,7 @@
        END PROGRAM MAINPRG.
       *----
       *----
-      * 01  FUNC-CALLER-FLAGS     PIC 9(01).          
+      * 01  FUNC-CALLER-FLAGS     PIC 9(01).
       *       88 ERROR-MSG                 VALUE 1.
       *       88 FILL-KEY                  VALUE 2.
       *       88 FILL-OUT-REC              VALUE 3.
@@ -177,7 +177,7 @@
       *----
       * UTILITY-FUNC.
       *     EVALUATE TRUE
-      *     WHEN FILL-KEY 
+      *     WHEN FILL-KEY
       *      MOVE INP-ID   TO SUB-IDX-ID OUT-ID
       *      MOVE INP-DVZ  TO SUB-IDX-DVZ OUT-DVZ
       *      MOVE INP-KEY  TO SUB-IDX-KEY
--- SUBPRG.cbl
+++ SUBPRG.cbl
@@ -13,7 +13,7 @@
       *RETURN CODE AMAC ARASTIR TAM OGREN
       * bosluklari kaldirinca zaten update oldu desin.
       *INITIAL ayarlamak gerekli mi ogren?
-       PROGRAM-ID.    SUBPRG
+       PROGRAM-ID.    SUBPRG IS INITIAL
        AUTHOR.        AYSU ONER.
        DATE-WRITTEN.  10/07/2023.
        DATE-COMPILED. 16/07/2023.
@@ -32,62 +32,62 @@
        FD  IDX-FILE.
        01  IDX-REC.
            05 IDX-KEY.
-              10  IDX-ID      PIC 9(05)    COMP-3.
-              10  IDX-DVZ     PIC 9(03)    COMP.
-           05 IDX-FIRSTN      PIC X(15).
-           05 IDX-LASTN       PIC X(15).
-           05 IDX-JUL         PIC 9(07)    COMP-3.
-           05 IDX-TUTAR       PIC 9(13)V99 COMP-3.
+             10  IDX-ID         PIC 9(05)    COMP-3.
+             10  IDX-DVZ        PIC 9(03)    COMP.
+           05 IDX-FIRSTN        PIC X(15).
+           05 IDX-LASTN         PIC X(15).
+           05 IDX-JUL           PIC 9(07)    COMP-3.
+           05 IDX-AMOUNT        PIC 9(13)V99 COMP-3.
        WORKING-STORAGE SECTION.
-       01  EXIT-FLAG          PIC X(01) VALUE 'N'.
        01  FILE-FLAGS.
-           05 IDX-ST          PIC 9(02).
-               88 IDX-SUCCESS VALUE 00 41 97.
-       01  SUB-FLAGS         PIC 9(01).
-           88  FUNC          VALUE 1.
+           05 IDX-ST            PIC 9(02).
+             88 IDX-SUCCESS              VALUE 00 97.
+           05 EXIT-ST           PIC 9(01).
+             88 EXIT-PROG                VALUE 1.
        01  REMOVE-SPACES-VAR.
            05 TMP-STR.
-            10 LEN            PIC  9(02).
-            10 CHARS          PIC  X(15).
-            10 I              PIC  9(02).
-            10 J              PIC  9(02).
-            10 K              PIC  9(02).
+             10 LEN             PIC  9(02).
+             10 CHARS           PIC  X(15).
+             10 I               PIC  9(02).
+             10 J               PIC  9(02).
+             10 K               PIC  9(02).
            05 RES-STR.
-            10 LEN            PIC 9(02).
-            10 CHARS          PIC X(15).
+             10 LEN             PIC 9(02).
+             10 CHARS           PIC X(15).
        LINKAGE SECTION.
        01  LN-SUB-IDX-KEY.
            05 LN-SUB-IDX-ID     PIC 9(05) COMP-3.
            05 LN-SUB-IDX-DVZ    PIC 9(03) COMP.
        01  LN-OUT-MSG-INFO.
-           05 LN-OUT-RROC-TYP     PIC X(09).
-           05 LN-OUT-RC            PIC 9(02).
-           05 LN-OUT-MSG           PIC X(20).
-           05 LN-NMFROM            PIC X(15).
-           05 LN-NMTO              PIC X(15).
-           05 LN-LSTFROM           PIC X(15).
-           05 LN-LSTTO             PIC X(15).
+           05 LN-OUT-RROC-TYP   PIC X(09).
+           05 LN-OUT-RC         PIC 9(02).
+           05 LN-OUT-MSG        PIC X(20).
+           05 LN-FIRSTNFROM     PIC X(15).
+           05 LN-FIRSTNTO       PIC X(15).
+           05 LN-LASTNFROM      PIC X(15).
+           05 LN-LASTNTO        PIC X(15).
       *------------
        PROCEDURE DIVISION USING LN-OUT-MSG-INFO, LN-SUB-IDX-KEY.
       *----
        FILE-OPEN-CONTROL.
            OPEN I-O IDX-FILE
+      *    BU DISPLAYI SIL ISIN BITINCE
+           DISPLAY 'ST: ' IDX-ST
            IF NOT IDX-SUCCESS
              DISPLAY '.VSAM FILE CANNOT OPEN: ' IDX-ST
-             MOVE 'Y' TO EXIT-FLAG
+             SET EXIT-PROG TO TRUE
              MOVE IDX-ST TO RETURN-CODE
-             SET FUNC TO TRUE
              PERFORM EXIT-SUBPROG
            END-IF.
-           MOVE LN-SUB-IDX-ID  TO IDX-ID.
-           MOVE LN-SUB-IDX-DVZ TO IDX-DVZ.
+      *     MOVE LN-SUB-IDX-ID  TO IDX-ID.
+      *     MOVE LN-SUB-IDX-DVZ TO IDX-DVZ.
            MOVE LN-SUB-IDX-KEY TO IDX-KEY.
        FILE-OPEN-CONTROL-END. EXIT.
       *----
-      *----
+      *-------------------------------------------------
            ENTRY 'READPROC' USING LN-OUT-MSG-INFO, LN-SUB-IDX-KEY.
+      *-------------------------------------------------
            PERFORM FILE-OPEN-CONTROL
-           MOVE '-READ-RC:' TO LN-OUT-RROC-TYP
            READ IDX-FILE KEY IS IDX-KEY
            INVALID KEY
              MOVE IDX-ST TO LN-OUT-RC
@@ -96,19 +96,20 @@
              MOVE IDX-ST TO LN-OUT-RC
              MOVE ' RECORD READ' TO LN-OUT-MSG
            END-READ.
-           SET FUNC TO TRUE.
+           MOVE '-READ-RC:' TO LN-OUT-RROC-TYP
+           SET EXIT-PROG TO TRUE.
            PERFORM EXIT-SUBPROG.
       *----
-      *----
+      *-------------------------------------------------
            ENTRY 'WRITPROC' USING LN-OUT-MSG-INFO, LN-SUB-IDX-KEY.
+      *-------------------------------------------------
            PERFORM FILE-OPEN-CONTROL
-           MOVE '-WRIT-RC:' TO LN-OUT-RROC-TYP
            READ IDX-FILE KEY IS IDX-KEY
            NOT INVALID KEY
              MOVE 'AYSU           ' TO IDX-FIRSTN
              MOVE 'ONER           ' TO IDX-LASTN
              MOVE '1995126'         TO IDX-JUL
-             MOVE '000000000000000' TO IDX-TUTAR
+             MOVE '000000000000000' TO IDX-AMOUNT
            END-READ
            WRITE IDX-REC
            INVALID KEY
@@ -118,26 +119,26 @@
               MOVE IDX-ST TO LN-OUT-RC
               MOVE ' NEW RECORD ADDED' TO LN-OUT-MSG
            END-WRITE.
-           SET FUNC TO TRUE.
+           MOVE '-WRIT-RC:' TO LN-OUT-RROC-TYP
+           SET EXIT-PROG TO TRUE.
            PERFORM EXIT-SUBPROG.
       *----
-      *----
+      *-------------------------------------------------
            ENTRY 'UPDTPROC' USING LN-OUT-MSG-INFO, LN-SUB-IDX-KEY.
+      *-------------------------------------------------
            PERFORM FILE-OPEN-CONTROL
-           MOVE '-UPDT-RC:' TO LN-OUT-RROC-TYP
            READ IDX-FILE KEY IS IDX-KEY
            NOT INVALID KEY
       *****updateforname
-             MOVE IDX-FIRSTN TO CHARS OF TMP-STR LN-NMFROM
+             MOVE IDX-FIRSTN TO CHARS OF TMP-STR LN-FIRSTNFROM
              PERFORM REMOVE-SPACES
              PERFORM REPLACING-CHR
-             MOVE CHARS OF RES-STR TO IDX-FIRSTN LN-NMTO
+             MOVE CHARS OF RES-STR TO IDX-FIRSTN LN-FIRSTNTO
       *****updateforlastname
-             MOVE IDX-LASTN TO CHARS OF TMP-STR LN-LSTFROM
+             MOVE IDX-LASTN TO CHARS OF TMP-STR LN-LASTNFROM
              PERFORM REMOVE-SPACES
              PERFORM REPLACING-CHR
-             MOVE CHARS OF RES-STR TO IDX-LASTN LN-LSTTO
-      *****
+             MOVE CHARS OF RES-STR TO IDX-LASTN LN-LASTNTO
            END-READ.
            REWRITE IDX-REC
            INVALID KEY
@@ -147,13 +148,14 @@
              MOVE IDX-ST TO LN-OUT-RC
              MOVE ' RECORD UPDATED' TO LN-OUT-MSG
            END-REWRITE.
-           SET FUNC TO TRUE.
+           MOVE '-UPDT-RC:' TO LN-OUT-RROC-TYP
+           SET EXIT-PROG TO TRUE.
            PERFORM EXIT-SUBPROG.
       *----
-      *----
+      *-------------------------------------------------
            ENTRY 'DELTPROC' USING LN-OUT-MSG-INFO, LN-SUB-IDX-KEY.
+      *-------------------------------------------------
            PERFORM FILE-OPEN-CONTROL
-           MOVE '-DELT-RC:' TO LN-OUT-RROC-TYP
            DELETE IDX-FILE RECORD
            INVALID KEY
              MOVE IDX-ST TO LN-OUT-RC
@@ -162,7 +164,8 @@
              MOVE IDX-ST TO LN-OUT-RC
              MOVE ' RECORD DELETED' TO LN-OUT-MSG
            END-DELETE.
-           SET FUNC TO TRUE.
+           MOVE '-DELT-RC:' TO LN-OUT-RROC-TYP
+           SET EXIT-PROG TO TRUE.
            PERFORM EXIT-SUBPROG.
       *----
       *----
@@ -178,7 +181,8 @@
                WITH POINTER I
              END-UNSTRING
              ADD J TO K GIVING J
-           END-PERFORM.
+           END-PERFORM
+           COMPUTE LEN OF RES-STR = J - 1.
        REMOVE-SPACES-END. EXIT.
       *----
       *----
@@ -192,14 +196,20 @@
       *----
       *----
        EXIT-SUBPROG.
-           IF FUNC
-              GOBACK
+           IF EXIT-PROG
+               DISPLAY IDX-ID IDX-DVZ
+      *         CLOSE IDX-FILE
+               GOBACK
            END-IF.
        EXIT-SUBPROG-END. EXIT.
+      *
+      *
+      *
       *----
-           ENTRY 'PRGEXIT'.
-           IF EXIT-FLAG = 'Y' THEN
-               CLOSE IDX-FILE
-               EXIT PROGRAM
-           END-IF.
-       END PROGRAM SUBPRG.
+      *     ENTRY 'PRGEXIT'.
+      *     IF CLOSE-FILE
+      *         DISPLAY 'GIRDI' IDX-ID IDX-DVZ
+      *         CLOSE IDX-FILE
+      *         EXIT PROGRAM
+      *     END-IF.
+      * END PROGRAM SUBPRG.
